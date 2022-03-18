1c024c2058ca7c4adb0da6ca87e6589b2e5cbac4
Meregalli Amos <meregalli.amos@osdbsesto>
2022-03-18 14:56:32 +0100

    Created Folders

---
+++ Register/bookmarks.js
@@ -0,0 +1,30 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Username",
+    links: [
+      { name: "Inbox", url: "https://inbox.google.com" },
+      { name: "GitHub", url: "https://github.com" },
+      { name: "Drive", url: "https://drive.google.com" },
+    ],
+  },
+  {
+    title: "Password",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "Netflix", url: "https://netflix.com" },
+      { name: "Crunchyroll", url: "https://crunchyroll.com" },
+      {
+        name: "Amazon Prime",
+        url: "https://www.amazon.com/Amazon-Video",
+      },
+    ],
+  },
+  {
+    title:"Confirm",
+    links: [
+      { name: "Twitter", url: "https://twitter.com" },
+      { name: "Facebook", url: "https://facebook.com" },
+    ],
+  },
+];
